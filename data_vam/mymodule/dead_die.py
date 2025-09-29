fbd6f8bb4868fc1c3036f45183fa97b8b24ff089
coob <koosanghwi@gmail.com>
2025-09-29 21:58:39 +0900

    ver 1.07

--- data_vam/mymodule/dead_die.py
+++ data_vam/mymodule/dead_die.py
@@ -53,7 +53,7 @@ def dead_check(cla):
                 full_path = "c:\\my_games\\vam\\data_vam\\imgs\\dead_die\\recovery_btn.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.8)
+                imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
                     print("recovery_btn", imgs_)
                     is_dead = True
@@ -123,7 +123,7 @@ def dead_recovery(cla):
             full_path = "c:\\my_games\\vam\\data_vam\\imgs\\dead_die\\recovery_btn.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.8)
+            imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
                 print("recovery_btn", imgs_)
                 dead_recovery_btn(cla)
@@ -229,7 +229,7 @@ def dead_recovery_btn(cla):
                 full_path = "c:\\my_games\\vam\\data_vam\\imgs\\dead_die\\recovery_btn.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.8)
+                imgs_ = imgs_set_(0, 30, 300, 300, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
                     print("recovery_btn", imgs_)
                     click_pos_reg(imgs_.x, imgs_.y, cla)
