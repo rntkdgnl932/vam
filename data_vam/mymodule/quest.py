2490f8e5c4c1c8b59f215bfe20e4ebbcd94c6f56
coob <koosanghwi@gmail.com>
2025-09-29 22:15:19 +0900

    ver 1.071

--- data_vam/mymodule/quest.py
+++ data_vam/mymodule/quest.py
@@ -176,6 +176,22 @@ def quest_get(cla, data):
                                     if imgs_ is not None and imgs_ != False:
                                         print("soolock_btn", imgs_)
                                         click_pos_reg(imgs_.x, imgs_.y, cla)
+
+
+                                        for q in range(10):
+                                            full_path = "c:\\my_games\\vam\\data_vam\\imgs\\quest\\anymore_quest.PNG"
+                                            img_array = np.fromfile(full_path, np.uint8)
+                                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                            imgs_ = imgs_set_(370, 80, 600, 140, cla, img, 0.7)
+                                            if imgs_ is not None and imgs_ != False:
+                                                print("anymore_quest", imgs_)
+                                                click_pos_2(300, 190, cla)
+                                                break
+                                            QTest.qWait(100)
+
+
+
+
                                     else:
                                         complete = True
                                         break
